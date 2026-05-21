7ffbd03e57c9b4084fd3860d44a6d12f1d967380
ColourfulPizza <41532728+ColourfulPizza@users.noreply.github.com>
2026-05-21 22:31:07 +0100

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+const int INF = -1e9;
+class Solution {
+public:
+
+    int getMinIndex(vector<ListNode*>& lists)
+    {
+        int sz = lists.size();
+        int curMin = -1;
+        for (int i = 0; i < sz; i++)
+        {
+            if (lists[i] == nullptr)
+                continue;
+            if (curMin == -1)
+                curMin = i;
+            if (lists[curMin] -> val > lists[i] -> val)
+                curMin = i;
+        }
+        return curMin;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int sz = lists.size();
+
+        if (sz == 0)
+            return nullptr;
+        
+        int par = getMinIndex(lists);
+        if (par == -1)
+            return nullptr;
+
+        ListNode* parPtr = lists[par];
+        ListNode* first = parPtr;
+        lists[par] = lists[par] -> next;
+        while (true)
+        {
+            // get min
+            int cur = getMinIndex(lists);
+            if (cur == -1 || lists[cur] == nullptr)
+                break;
+            parPtr -> next = lists[cur];
+            parPtr = lists[cur];
+            par = cur;
+            lists[cur] = lists[cur] -> next;
+            // int par = getMinIndex(lists);
+            // lists[par]
+        }
+
+        return first;
+    }
+};
