ba7b569d7df6004af1ba53d53393d27227d7d784
ColourfulPizza <41532728+ColourfulPizza@users.noreply.github.com>
2026-05-25 17:19:47 +0100

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<bool> reach(n);
+
+        int mx = 0;
+        reach[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            cout << " we're at " << i << endl;
+            if (!reach[i]) continue;
+            for (int j = mx - i + 1; j <= nums[i] && i + j < n; j++)
+            {
+                reach[i + j] = 1;
+                cout << "can reach " << i + j << " from " << i << endl;
+            }
+            mx = max(mx, i + nums[i]);
+        }
+
+        return reach[n - 1];
+    }
+};
